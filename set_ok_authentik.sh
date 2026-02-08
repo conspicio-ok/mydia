c17b498e8cbe6adf46d21a66fc3780bd9c4ae11f
conspicio <raphael.vingt.cinq@ikmail.com>
2026-02-08 13:46:14 +0100

    set_ok_authentik

---
+++ set_ok_authentik.sh
@@ -0,0 +1,5 @@
+# L'utilisateur dans le conteneur Authentik a l'UID 1000
+sudo chown -R 1000:1000 ./data/authentik/media
+sudo chown -R 1000:1000 ./data/authentik/templates
+sudo chown -R 1000:1000 ./data/authentik/certs
+
